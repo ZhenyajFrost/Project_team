476a6414dfff26ed9eb39753542c13ae833d86cc
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-01-26 22:10:10 +0300

    LOTS of stuff
    formated lots to look as those guys requested

--- ClientApp/src/components/Lot/Lot.js
+++ ClientApp/src/components/Lot/Lot.js
@@ -1,18 +1,69 @@
 ﻿import React, { useState } from "react";
 import { useEffect } from "react";
 import "./style.css";
+import MoneySvg from "./attach_money.svg";
 import { NavLink } from "react-router-dom";
-export const formatTime=(seconds)=>{
-    let minutes=Math.floor(seconds/60);
-    seconds=seconds%60;
-    let hours=Math.floor(minutes/60);
-    minutes=minutes%60;
-    let days = Math.floor(hours/24);
-    hours=hours%24;
-    return"kffgh";
-    return{days, hours, minutes, seconds}
-}
-
+const vidminDays = (days) => {
+  days = days.toString();
+  const dlen = days.length;
+  const lnum = Number(days[dlen - 1]);
+  return dlen > 1 && days[dlen - 2] !== 1
+    ? "днів"
+    : lnum === 1
+    ? "день"
+    : lnum < 5 && lnum > 0
+    ? "дні"
+    : "днів";
+};
+const vidminHours = (hours) => {
+  switch (hours) {
+    case 1:
+    case 21:
+      return "година";
+    case 2:
+    case 3:
+    case 4:
+    case 22:
+    case 23:
+    case 24:
+      return "години";
+    default:
+      return "годин";
+  }
+};
+const vidminMinutes = (minutes) => {
+  minutes = minutes.toString();
+  const dlen = minutes.length;
+  const lnum = Number(minutes[dlen - 1]);
+  return dlen > 1 && minutes[dlen - 2] === 1
+    ? "хвилин"
+    : lnum === 1
+    ? "хвилина"
+    : lnum < 5 && lnum > 0
+    ? "хвилини"
+    : "хвилин";
+};
+export const formatTime = (seconds) => {
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  let hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  let days = Math.floor(hours / 24);
+  hours = hours % 24;
+  return (
+    days +
+    " " +
+    vidminDays(days) +
+    " " +
+    hours +
+    " " +
+    vidminHours(hours) +
+    " " +
+    minutes +
+    " " +
+    vidminMinutes(minutes)
+  );
+};
 
 function Lot({
   id,
@@ -23,29 +74,45 @@ function Lot({
   timeTillEnd,
   hot,
   imageURL,
-  openModal
+  openModal,
 }) {
-  
-  const [ttl, setTtl]=useState(timeTillEnd);
-  useEffect(()=>{
-    setTimeout(()=>{
-      setTtl(ttl-1);
-
-    }, 1000)
-  }, [ttl])
+  const [ttl, setTtl] = useState(timeTillEnd);
+  useEffect(() => {
+    setTimeout(() => {
+      setTtl(ttl - 1);
+    }, 1000);
+  }, [ttl]);
   return (
-    <div className={hot ? "lot hot" : "lot"}>
-      <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzj49rb70qayLcsE_g-Bl54iw3sMoJsZRfLbU-tQOqWQ&s"} className="lot-image" />
-      <h3 className="lot-title">{title}</h3>
+    <div className="lot">
+      <img
+        src={
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzj49rb70qayLcsE_g-Bl54iw3sMoJsZRfLbU-tQOqWQ&s"
+        }
+        className="lot-image"
+        alt="oleg"
+      />
+      <div className="lot-text">
+        <h3 className="lot-title">{title}</h3>
+        <p className="lot-desc">{shortDescription}</p>
 
-      <div className="lot-info">
-        <p className="lot-desc">Desc:{shortDescription}</p>
-        <p className="lot-category">Сат:{category}</p>
-
-        <p className="lot-price">Price:{price}</p>
-        <p className="lot-time">{formatTime(timeTillEnd)}</p>
-        <NavLink to={"/lot/" + id}>Details</NavLink>
-        <button onClick={openModal}>Edit</button>
+        <div className="lot-info">
+          <p>
+            <svg>
+              <use href={`${MoneySvg}#attach_money`} />
+            </svg>
+            {price}
+          </p>
+          <p>
+            <svg>
+              <use href={`${MoneySvg}#schedule`} />
+            </svg>
+            {formatTime(timeTillEnd)}
+          </p>
+           <NavLink to={"/lot/" + id} className="arrow-outward"><svg>
+            <use href={`${MoneySvg}#arrow_outward`}/> 
+           </svg></NavLink>
+          
+        </div>
       </div>
     </div>
   );
--- ClientApp/src/components/UI/LotContainer/LotContainer.js
+++ ClientApp/src/components/UI/LotContainer/LotContainer.js
@@ -1,31 +1,55 @@
-import React, { useRef, useImperativeHandle, forwardRef, useState } from 'react'
-import Lot from '../../Lot/Lot.js'
-import ModalWindow from '../../ModalWindow/ModalWindow.js';
-import LotChange from '../../LotChange/LotChange.js';
-import Button from '../Button/Button.js';
-import classes from './LotContainer.module.css'
+import React, {
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+} from "react";
+import Lot from "../../Lot/Lot.js";
+import ModalWindow from "../../ModalWindow/ModalWindow.js";
+import LotChange from "../../LotChange/LotChange.js";
+import Button from "../Button/Button.js";
+import classes from "./LotContainer.module.css";
+import CategoryContainer from "../../CategoryContainer/CategoryContainer.js";
 
 function LotContainer({ lots, setLots }) {
-    const [selectedId, setSelectedId]=useState(-1);
-    return (
-        <div className={classes.lotsContainer}>
-            {lots.map((lot, i) =>
-                <Lot id={lot.id}
-                    title={lot.title}
-                    price={lot.price}
-                    shortDescription={lot.shortDescription}
-                    category={lot.category}
-                    timeTillEnd={lot.timeTillEnd}
-                    hot={lot.hot}
-                    imageURL={lot.imageURL}
-                    openModal={()=>setSelectedId(i)}
-                />)}
-            <ModalWindow visible={selectedId>=0} setVisible={setSelectedId}>
-                <LotChange lot={lots[selectedId]} setLot={(data)=>setLots([...lots.filter((_, i)=>i!==selectedId), data])} kms={() => setSelectedId(-1)}/>
-                <Button onClick={() => setSelectedId(-1)}>Modal Window Close</Button>
-            </ModalWindow>
-        </div>
-    )
+  //const [selectedId, setSelectedId] = useState(-1);
+  const [selectedCat, setSelectedCat] = useState("Холодильники");
+  return (
+    <div>
+    <h2>Популярні лоти</h2>
+    <CategoryContainer categories={["Холодильники","Іфон 13","Картини","Телевізор","Іграшки","Навушники","Колеса)"]} onCategoryChange={setSelectedCat} selectedCategorie={selectedCat}/>
+    
+      <div className={classes.lotsContainer}>
+        {lots.map((lot, i) => (
+          <Lot
+            id={lot.id}
+            title={lot.title}
+            price={lot.price}
+            shortDescription={lot.shortDescription}
+            category={lot.category}
+            timeTillEnd={lot.timeTillEnd}
+            hot={lot.hot}
+            imageURL={lot.imageURL}
+            //openModal={() => setSelectedId(i)}
+          />
+        ))}
+
+      </div>
+    </div>
+  );
 }
 
 export default LotContainer;
+
+
+
+        {/* <ModalWindow visible={selectedId >= 0} setVisible={setSelectedId}>
+          <LotChange
+            lot={lots[selectedId]}
+            setLot={(data) =>
+              setLots([...lots.filter((_, i) => i !== selectedId), data])
+            }
+            kms={() => setSelectedId(-1)}
+          />
+          <Button onClick={() => setSelectedId(-1)}>Modal Window Close</Button>
+        </ModalWindow> */}
