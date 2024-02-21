ab1b94f63dbf89abc45959856a74a121b1de6f3a
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-02-21 22:34:02 +0300

    now swappable horizontaly

--- ClientApp/src/components/MultiplePhotoSelector/MultiplePhotoSelector.js
+++ ClientApp/src/components/MultiplePhotoSelector/MultiplePhotoSelector.js
@@ -7,12 +7,36 @@ function MultiplePhotoSelector({ photos, setPhotos }) {
     if (photos[i]) {
       disp.push(photos[i]);
     } else {
-      if (photos[i - 1] || i===0) disp.push("+");
+      if ((photos[i - 1] && photos[i-1]!=="+") || i === 0) disp.push("+");
       else disp.push(null);
     }
   }
-  
-  const dispFin = disp.map((v, i) => <PhotoItem photo={v} setPhoto={(p)=>setPhotos([...disp.slice(0, i), p, ...disp.slice(i+1)])}/>);
+
+  const dispFin = disp.map((v, i) => (
+    <PhotoItem
+      photo={v}
+      order={(e) => {
+        console.log(e);
+        if (e > 0)
+          setPhotos([
+            ...disp.slice(0, i),
+            ...disp.slice(i + 1, i + e + 1),
+            v,
+            ...disp.slice(i + e + 1),
+          ]);
+        else
+          setPhotos([
+            ...disp.slice(0, i+e+1),
+            v,
+            ...disp.slice(i+e+1, i),
+            ...disp.slice(i + 1),
+          ]);
+      }}
+      setPhoto={(p) =>
+        setPhotos([...disp.slice(0, i), p, ...disp.slice(i + 1)])
+      }
+    />
+  ));
   return <div className={css.container}>{dispFin}</div>;
 }
 
--- ClientApp/src/components/MultiplePhotoSelector/PhotoItem.js
+++ ClientApp/src/components/MultiplePhotoSelector/PhotoItem.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import css from "./style.module.css";
 import { Buffer } from "buffer";
 
-function PhotoItem({ photo, setPhoto }) {
+function PhotoItem({ photo, setPhoto, order }) {
   let disp;
   const [drag, setDrag] = useState(false);
   const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [positionStart, setPositionStart] = useState({ top: 0, left: 0 });
   const addPhoto = () => {
     var input = document.createElement("input");
     input.type = "file";
@@ -38,16 +39,30 @@ function PhotoItem({ photo, setPhoto }) {
       );
   }
   const move = (e) => {
+    const left = e.clientX - e.target.width / 2;
+    const top = e.clientY + e.target.height / 2;
     setPosition({
-      top: e.clientY - e.target.height / 2,
-      left: e.clientX - e.target.width / 2,
+      top,
+      left,
     });
   };
   if (drag) {
     return (
       <div className={css.item}>
         <div
-          onMouseUp={() => setDrag(false)}
+          onMouseUp={(e) => {
+            setDrag(false);
+            const n = 0.8
+            let res = 0
+            if (
+              position.left - positionStart.left > e.target.width *n ||
+              position.left - positionStart.left < -e.target.width * n
+            ) {
+              res+=(position.left - positionStart.left) / (e.target.width * n)
+              
+            }
+            order(res)
+          }}
           onMouseMove={move}
           className={css.item + " " + css.dragable}
           style={{ top: position.top + "px", left: position.left + "px" }}
@@ -63,6 +78,10 @@ function PhotoItem({ photo, setPhoto }) {
           if (photo && photo !== "+") {
             setDrag(true);
             move(e);
+            setPositionStart({
+              left: e.clientX - e.target.width / 2,
+              top: e.clientY + e.target.height / 2,
+            });
           }
         }}
         className={css.item}
