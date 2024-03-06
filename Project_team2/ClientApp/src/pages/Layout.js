616cc5d1d4b961573c7d6141f140576f061aa6b5
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-06 19:42:11 +0200

    centered the page
    easy

--- Project_team2/ClientApp/src/pages/Layout.js
+++ Project_team2/ClientApp/src/pages/Layout.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import NavMenu  from '../components/UI/NavMenu/NavMenu';
-import Footer  from '../components/UI/Footer/Footer';
-import css from "./Layout.module.css"
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import NavMenu from "../components/UI/NavMenu/NavMenu";
+import Footer from "../components/UI/Footer/Footer";
+import css from "./Layout.module.css";
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
+  render() {
     return (
-      <div>
+      <div className={css.container1}>
         <NavMenu />
-        <Container className={css.container1}>
-          {this.props.children}
-        </Container>
+        <Container>{this.props.children}</Container>
         <Footer />
       </div>
     );
