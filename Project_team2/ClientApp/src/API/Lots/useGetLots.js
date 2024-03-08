295733fe072164280313d683c7baeb79385314f5
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-08 17:33:12 +0200

    added filter to getLots

--- Project_team2/ClientApp/src/API/Lots/useGetLots.js
+++ Project_team2/ClientApp/src/API/Lots/useGetLots.js
@@ -1,36 +1,35 @@
 import React, { useState } from "react";
-import axios from 'axios';
-import { LOTS_ENDPOINT } from '../apiConstant';
+import axios from "axios";
+import { LOTS_ENDPOINT } from "../apiConstant";
 
 const useGetLots = () => {
-    const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-    const [error, setError] = useState(null);
-    const [lots, setLots] = useState([]);
-    const [totalCount, setTotalCount] = useState();
+  const [error, setError] = useState(null);
+  const [lots, setLots] = useState([]);
+  const [totalCount, setTotalCount] = useState();
 
-    const getLots = async (page, pageSize) => { //FILTER
-        setLoading(true);
-        try {
-            const response = await axios.get(`${LOTS_ENDPOINT}/getAllLots`, {
-                params:
-                {
-                    page: page,
-                    pageSize: pageSize
-                }
-            }); //FILTER IS NEEDED
-            console.log('Lots successfully retrieved: ', response.data);
-            setLots(response.data.lots);
-            setTotalCount(response.data.totalCount)
-        } catch (error) {
-            console.error('Getting lots failed: ', error);
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return [getLots, lots, totalCount, isLoading, error];
+  const getLots = async (page, pageSize, filter={}) => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`${LOTS_ENDPOINT}/getAllLots`, {
+        params: {
+          page: page,
+          pageSize: pageSize,
+          ...filter
+        },
+      }); 
+      //console.log("Lots successfully retrieved: ", response.data);
+      setLots(response.data.lots);
+      setTotalCount(response.data.totalCount);
+    } catch (error) {
+      //console.error("Getting lots failed: ", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return [getLots, lots, totalCount, isLoading, error];
 };
 
 export default useGetLots;
