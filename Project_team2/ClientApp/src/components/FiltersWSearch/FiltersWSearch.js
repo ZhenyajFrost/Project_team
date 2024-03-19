f86a96116d8a80932b1a1d18643724720c948741
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-19 22:06:50 +0200

    added sort to FWS
    +minor styling

--- Project_team2/ClientApp/src/components/FiltersWSearch/FiltersWSearch.js
+++ Project_team2/ClientApp/src/components/FiltersWSearch/FiltersWSearch.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import css from "./FiltersWSearch.module.css";
 import Notiflix from "notiflix";
-import InputSearch from '../UI/Input/InputSearch'
+import InputSearch from "../UI/Input/InputSearch";
 
 function FiltersWSearch({ onChange, initial }) {
-  console.log(initial)
   const [filters, setFilters] = useState({
     searchQuery: null,
     minPrice: null,
     maxPrice: null,
     endTill: null,
-    ...initial
+    ...initial,
   });
 
   useEffect(() => {
@@ -18,19 +17,17 @@ function FiltersWSearch({ onChange, initial }) {
     const minPrice = Number(filters.minPrice);
     const maxPrice = Number(filters.maxPrice);
     if (isNaN(minPrice) || isNaN(maxPrice)) {
-      Notiflix.Notify.failure(
-        "Будь ласка, не вийобуйтесь і оберіть нормальну ціну."
-      );
+      Notiflix.Notify.failure("Будь ласка оберіть нормальну ціну.");
       return;
     }
 
     if (minPrice < 0) {
-      setFilters(prev => ({ ...prev, minPrice: (minPrice * -1)}));
+      setFilters((prev) => ({ ...prev, minPrice: minPrice * -1 }));
       return;
     }
 
     if (maxPrice < 0) {
-      setFilters(prev => ({ ...prev, maxPrice: maxPrice * -1 }));
+      setFilters((prev) => ({ ...prev, maxPrice: maxPrice * -1 }));
       return;
     }
     if (maxPrice < minPrice) {
@@ -41,23 +38,26 @@ function FiltersWSearch({ onChange, initial }) {
     //перевірка дати
     //хз як з датою працюватимемо (об'єкт, стрінг чи щось тому подібне), тож залишу це пустим
 
-    console.log("Filters changed in FiltersWSearch")
-    onChange(filters)
+    onChange(filters);
   }, [filters]);
 
   const onSearch = (value) => {
-    setFilters(prev => ({
+    setFilters((prev) => ({
       ...prev,
-      searchQuery: value
+      searchQuery: value,
     }));
-  }
-  
+  };
 
   return (
     <div className={css.filterContainer}>
       <div className={css.filterItem}>
         Пошук
-        <InputSearch onSearch={onSearch} value={filters.searchQuery} placeholder="Введіть будь-яку позицію" nobutton />
+        <InputSearch
+          onSearch={onSearch}
+          value={filters.searchQuery}
+          placeholder="Введіть будь-яку позицію"
+          nobutton
+        />
       </div>
       <div className={css.filterItem}>
         Ціна
@@ -78,7 +78,6 @@ function FiltersWSearch({ onChange, initial }) {
             <input
               type="number"
               value={filters.maxPrice}
-
               onChange={(e) => {
                 setFilters({ ...filters, maxPrice: e.target.value });
               }}
@@ -97,12 +96,51 @@ function FiltersWSearch({ onChange, initial }) {
           <option>Всі оголошення</option>
         </select>
       </div>
-      <div className={css.filterItem}>
-        Сортувати за:{" "}
-        <select
-          className={css.inputEl}>
-          <option>Як ми це зробимо?</option>
-        </select>
+      <div>
+        Сортувати за:
+        <div
+          className={css.inputEl}
+          style={{ display: "flex", overflow: "hidden" }}
+        >
+          <select
+            onChange={(v) => {
+              const [orderBy, ascending] = v.target.value.split(" ");
+              setFilters({
+                ...filters,
+                orderBy,
+                ascending: ascending === "true",
+              });
+            }}
+          >
+            <option value="">Довільний порядок</option>
+            <option value="title true">За назвою вверх</option>
+            <option value="title false">За назвою вниз</option>
+            <option value="category true">За категорією вверх</option>
+            <option value="category false">За категорією вниз</option>
+            <option value="timeTillEnd true">
+              За часом до закінчення вверх
+            </option>
+            <option value="timeTillEnd false">
+              За часом до закінчення вниз
+            </option>
+            <option value="minPrice true">За мінімальною ціною вверх</option>
+            <option value="minPrice false">За мінімальною ціною вниз</option>
+            <option value="minStepPrice true">
+              За мінімальним кроком ціни вверх
+            </option>
+            <option value="minStepPrice false">
+              За мінімальним кроком ціни вниз
+            </option>
+            <option value="price true">За ціною вверх</option>
+            <option value="price false">За ціною вниз</option>
+            <option value="isNew true">За новизною вверх</option>
+            <option value="isNew false">За новизною вниз</option>
+            <option value="region true">За регіоном вверх</option>
+            <option value="region false">За регіоном вниз</option>
+            <option value="city true">За містом вверх</option>
+            <option value="city false">За містом вниз</option>
+          </select>
+        </div>
       </div>
     </div>
   );
