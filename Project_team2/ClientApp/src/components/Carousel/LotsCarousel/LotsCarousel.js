699ddd5902c5d0094b3d6c9aef9e8e6886ec96a5
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-19 19:01:59 +0200

    they loop:)

--- Project_team2/ClientApp/src/components/Carousel/LotsCarousel/LotsCarousel.js
+++ Project_team2/ClientApp/src/components/Carousel/LotsCarousel/LotsCarousel.js
@@ -1,22 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Lot from "../../Lot/Lot";
 import styles from "../Carousel.module.css";
 
 function LotsCarousel({ lots, lotWidth = 600 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(()=>{
+    console.log(currentIndex);
+    if(currentIndex<0){
+        setCurrentIndex(lots.length-1);
+    }
+    if(currentIndex>lots.length-1){
+        setCurrentIndex(0);
+    }
+  }, [currentIndex])
   return (
     <div>
       <div className={styles.header}>
         <div className={styles.buttons}>
-          <button className={styles.prevBtn} onClick={()=>{setCurrentIndex(currentIndex+1)}}>
+          <button className={styles.prevBtn} onClick={()=>{setCurrentIndex(currentIndex-1)}}>
             &#10094;
           </button>
-          <button className={styles.nextBtn} onClick={()=>{setCurrentIndex(currentIndex-1)}}>
+          <button className={styles.nextBtn} onClick={()=>{setCurrentIndex(currentIndex+1)}}>
             &#10095;
           </button>
         </div>
       </div>
-      <div style={{ display: "flex", gap: "20px", transform:`translate(${currentIndex*lotWidth}px,0)`, transition:"all ease 1s" }}>
+      <div style={{ display: "flex", gap: "20px", transform:`translate(${-currentIndex*lotWidth}px,0)`, transition:"all ease 1s" }}>
         {lots.map((lot) => (
           <Lot
             key={lot.id}
