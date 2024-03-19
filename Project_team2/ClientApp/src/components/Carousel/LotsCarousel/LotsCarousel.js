a0b4e919c9a4ef44d7c5b0c78ab31526dc9bada1
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-19 18:59:21 +0200

    spec carousel for lots

---
+++ Project_team2/ClientApp/src/components/Carousel/LotsCarousel/LotsCarousel.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import Lot from "../../Lot/Lot";
+import styles from "../Carousel.module.css";
+
+function LotsCarousel({ lots, lotWidth = 600 }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <div>
+      <div className={styles.header}>
+        <div className={styles.buttons}>
+          <button className={styles.prevBtn} onClick={()=>{setCurrentIndex(currentIndex+1)}}>
+            &#10094;
+          </button>
+          <button className={styles.nextBtn} onClick={()=>{setCurrentIndex(currentIndex-1)}}>
+            &#10095;
+          </button>
+        </div>
+      </div>
+      <div style={{ display: "flex", gap: "20px", transform:`translate(${currentIndex*lotWidth}px,0)`, transition:"all ease 1s" }}>
+        {lots.map((lot) => (
+          <Lot
+            key={lot.id}
+            id={lot.id}
+            title={lot.title}
+            price={lot.price}
+            shortDescription={lot.shortDescription}
+            category={lot.category}
+            timeTillEnd={lot.timeTillEnd}
+            hot={lot.hot}
+            imageURLs={lot.imageURLs}
+            isAdmin={false}
+            isApproved={lot.approved}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LotsCarousel;
