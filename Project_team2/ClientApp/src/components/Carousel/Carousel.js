9911bb96986cfbd6cc0ec3303d65dded51d55247
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-19 22:37:43 +0200

    carousel fix
    IT GLOWS

--- Project_team2/ClientApp/src/components/Carousel/Carousel.js
+++ Project_team2/ClientApp/src/components/Carousel/Carousel.js
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
-import styles from './Carousel.module.css'; // Import the CSS module
+import React, { useState, useEffect } from "react";
+import styles from "./Carousel.module.css"; // Import the CSS module
 
 const Carousel = ({ items, maxItems = 4, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const visibleItems = maxItems;
 
   const next = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex < items.length - visibleItems ? prevIndex + 1 : prevIndex
-    );
+    console.log("dskdkdkkd");
+    setCurrentIndex((prevIndex) => prevIndex + 1);
   };
 
   const prev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+    setCurrentIndex((prevIndex) => prevIndex - 1);
   };
+  useEffect(() => {
+    if (currentIndex < 0) {
+      setCurrentIndex(0);
+    }
+    if (currentIndex > 1) {
+      setCurrentIndex(1);
+    }
+  }, [currentIndex]);
 
   return (
     <div>
       <div className={styles.header}>
         <h2>{title}</h2>
         <div className={styles.buttons}>
-          <button className={styles.prevBtn} onClick={prev}>&#10094;</button>
-          <button className={styles.nextBtn} onClick={next}>&#10095;</button>
+          <button className={styles.prevBtn} onClick={prev}>
+            &#10094;
+          </button>
+          <button className={styles.nextBtn} onClick={next}>
+            &#10095;
+          </button>
         </div>
-
       </div>
       <div className={styles.carouselContainer}>
         <div
           className={styles.carouselSlide}
-          style={{ transform: `translateX(-${currentIndex * (100 / visibleItems)}%)` }}
+          style={{
+            transform: `translateX(-${currentIndex * (100 / visibleItems)}%)`,
+          }}
         >
           {items.map((item, index) => (
             <div key={index} className={styles.carouselItem}>
@@ -36,10 +48,9 @@ const Carousel = ({ items, maxItems = 4, title }) => {
             </div>
           ))}
         </div>
-
+        <div className={styles.shine}></div>
       </div>
     </div>
-
   );
 };
 
