c4993620f2e1db698b637702b1a2abc3823d01d6
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-24 12:01:22 +0200

    fixed error on main page

--- Project_team2/ClientApp/src/components/Lot/LotSmall/LotSmall.js
+++ Project_team2/ClientApp/src/components/Lot/LotSmall/LotSmall.js
@@ -26,7 +26,7 @@ function LotSmall({
   const history = useHistory();
  const {token, user} = store();
 
-  const isLiked = user.likedLotIds.includes(id);
+  const isLiked = user ? user.likedLotIds.includes(id) : false;
 
   const [ttl, setTtl] = useState((new Date(timeTillEnd) - new Date()) / 1000);
   const { deleteLot, isLoading, error } = useDeleteLot();
