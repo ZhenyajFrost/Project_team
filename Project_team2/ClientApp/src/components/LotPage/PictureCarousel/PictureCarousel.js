8a7022b5192db9358155285c8ba6e206dd166f5d
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-25 15:45:13 +0200

    fix carousel adaptivee

--- Project_team2/ClientApp/src/components/LotPage/PictureCarousel/PictureCarousel.js
+++ Project_team2/ClientApp/src/components/LotPage/PictureCarousel/PictureCarousel.js
@@ -1,25 +1,24 @@
-import React, {useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { nanoid } from "nanoid";
 import css from "./style.module.css";
 import svg from "../../../images/svgDef.svg";
 
 function PictureCarousel({ images }) {
-  const [order, setOrder] = useState(images);
+  const [order, setOrder] = useState(images.filter((v) => v));
   const line = useRef(null);
   const lineHigh = useRef(null);
-
   const setTarget = (target) => {
     const pos = order.indexOf(target);
     const dist = order.length - pos - 1;
     if (dist < pos) {
-        const moveBkwrd = (i) => {
-            if (i <= dist) {
-              prev();
-              setTimeout(() => moveBkwrd(i + 1), (i+1)*750);
-            }
-          };   
-       
-          moveBkwrd(0);
+      const moveBkwrd = (i) => {
+        if (i <= dist) {
+          prev();
+          setTimeout(() => moveBkwrd(i + 1), (i + 1) * 750);
+        }
+      };
+
+      moveBkwrd(0);
     } else {
       const moveFwrd = (i) => {
         if (i > 0) {
@@ -28,49 +27,52 @@ function PictureCarousel({ images }) {
         }
       };
 
-   
-        moveFwrd(pos);
-      
+      moveFwrd(pos);
     }
   };
 
   const next = () => {
     line.current.style.transition = "all 0.5s ease";
-    line.current.style.transform = "translate(-400px, 0)";
+    line.current.style.transform = "translate(-40vw, 0)";
 
     lineHigh.current.style.transition = "all 0.5s ease";
-    lineHigh.current.style.transform = "translate(-1200px, 0)";
+    lineHigh.current.style.transform = "translate(-120vw, 0)";
     setTimeout(() => {
       order.push(order.shift());
       setOrder([...order]);
 
       line.current.style.transition = "none";
-      line.current.style.transform = "translate(-200px, 0)";
+      line.current.style.transform = "translate(-20vw, 0)";
 
       lineHigh.current.style.transition = "none";
-      lineHigh.current.style.transform = "translate(-600px, 0)";
+      lineHigh.current.style.transform = "translate(-60vw, 0)";
     }, 500);
   };
   const prev = () => {
     line.current.style.transition = "all 0.5s ease";
-    line.current.style.transform = "translate(0px, 0)";
+    line.current.style.transform = "translate(0vw, 0)";
 
     lineHigh.current.style.transition = "all 0.5s ease";
-    lineHigh.current.style.transform = "translate(0px, 0)";
+    lineHigh.current.style.transform = "translate(0vw, 0)";
     setTimeout(() => {
       order.unshift(order.pop());
       setOrder([...order]);
 
       line.current.style.transition = "none";
-      line.current.style.transform = "translate(-200px, 0)";
+      line.current.style.transform = "translate(-20vw, 0)";
 
       lineHigh.current.style.transition = "none";
-      lineHigh.current.style.transform = "translate(-600px, 0)";
+      lineHigh.current.style.transform = "translate(-60vw, 0)";
     }, 500);
   };
-
   if (order.length <= 1) {
-    return <img src={order[0]} alt="highlight" className={css.selectedImg} />;
+    return (
+      <img
+        src={order[0]}
+        alt="highlight"
+        className={`${css.selectedImg} ${css.solo}`}
+      />
+    );
   }
   return (
     <div className={css.carouselContainer}>
@@ -88,13 +90,15 @@ function PictureCarousel({ images }) {
 
       <div className={css.roundabout}>
         <div className={css.line} ref={line}>
-          {order.map((v, i) => (
-            <img
-              key={nanoid()}
-              src={v}
-              alt="pic"
-              onClick={() => setTarget(v)}
-            />
+          {order.map((v) => (
+            <div className={css.imgContRound}>
+              <img
+                key={nanoid()}
+                src={v}
+                alt="pc"
+                onClick={() => setTarget(v)}
+              />
+            </div>
           ))}
           <img src={order[0]} alt="pic" />
         </div>
