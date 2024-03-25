ecd57a87035013428a088b56cae2caaf892f7fbb
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-03-26 00:22:20 +0200

    fin i genesis

--- Project_team2/ClientApp/src/components/Genesis/CategoryModal/CategoryModal.js
+++ Project_team2/ClientApp/src/components/Genesis/CategoryModal/CategoryModal.js
@@ -1,18 +1,25 @@
 import React from "react";
 import def from "../../../images/svgDef.svg";
-import css from "./style.module.css"
+import css from "./style.module.css";
 
 function CategoryModal({ categories, onSelect }) {
   return (
-    <div className={css.container}>
-      {categories.map((selectedCat) => (
-        <div key={selectedCat.id} className={css.item} onClick={()=>onSelect(selectedCat)}>
-          <svg>
-            <use href={`${def}#${selectedCat.imgId}`} />
-          </svg>
-          <p>{selectedCat.title}</p>
-        </div>
-      ))}
+    <div>
+      <h2 className={css.header}>Виберіть Категорію</h2>
+      <div className={css.container}>
+        {categories.map((selectedCat) => (
+          <div
+            key={selectedCat.id}
+            className={css.item}
+            onClick={() => onSelect(selectedCat)}
+          >
+            <svg>
+              <use href={`${def}#${selectedCat.imgId}`} />
+            </svg>
+            <p>{selectedCat.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
